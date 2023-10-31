9fba4ba44c82b139e3bce39166dfa4d0ae747473
YuryKuptsou <yury.kuptsou.mail@gmail.com>
2023-10-31 09:18:57 +0300

    Refactored filter.

--- Homework/Filters/ActionLoggingFilter.cs
+++ Homework/Filters/ActionLoggingFilter.cs
@@ -1,6 +1,7 @@
 ﻿using Homework.Entities.Configuration;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Options;
+using System.Text;
 
 namespace Homework.Filters
 {
@@ -18,18 +19,32 @@ namespace Homework.Filters
         public void OnActionExecuting(ActionExecutingContext context)
         {
             if (_filterLoggingSettings.Value.EnableLogging) {
-                _logger.LogInformation($"'{context.RouteData.Values["controller"]}/{context.RouteData.Values["action"]}' started.");
-                if (context.ActionArguments.Any()) {
-                    _logger.LogInformation($"Action arguments: {string.Join(";", context.ActionArguments.Select(x => $"{x.Key} = {x.Value}"))}");
-                }
+
+                _logger.LogInformation($"{GetControllerInfo(context.RouteData)} started. Action arguments: {GetActionArgumnetsForLogging(context)}.");
             }
         }
 
         public void OnActionExecuted(ActionExecutedContext context)
         {
             if (_filterLoggingSettings.Value.EnableLogging) {
-                _logger.LogInformation($"'{context.RouteData.Values["controller"]}/{context.RouteData.Values["action"]}' completed.");
+                _logger.LogInformation($"{GetControllerInfo(context.RouteData)} completed.");
             }
+        }
+
+        private string GetControllerInfo(RouteData routeData)
+        {
+            return $"'{routeData.Values["controller"]}/{routeData.Values["action"]}'";
+        }
+
+        private string GetActionArgumnetsForLogging(ActionExecutingContext context)
+        {
+            string actionArguments = $"{string.Join(";", context.ActionArguments.Select(x => $"{x.Key} = {x.Value}"))}";
+
+            if (string.IsNullOrEmpty(actionArguments)) {
+                actionArguments = "No action arguments";
+            }
+
+            return actionArguments;
         }
     }
 }
